17b92a0014dba2c381ac8af20ea543de2d6556d3
benpearce1 <ben.pearce@octopus.com>
2020-01-24 16:01:50 +1000

    webapi version

---
+++ source/webapi/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration config;
+
+        public ValuesController(IConfiguration config) {
+            this.config = config;
+        }
+        
+        [HttpGet()]
+        public async Task<IActionResult> Get() {
+
+            var connString = config.GetConnectionString("Db");
+            using (var connection = new SqlConnection(connString)) {
+                try {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = "select * from sys.tables";
+                    var reader = command.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                    var count = 0;
+                    while(reader.Read())
+                    {
+                        count++;
+                    }
+
+                    return Ok(count);
+                }
+                catch(Exception ex)
+                 {
+                     throw new InvalidOperationException(ex.ToString());
+                 }
+            }
+            return Ok();
+        }
+    }
+}
---
+++ source/webapi/build.cmd
@@ -0,0 +1,2 @@
+dotnet publish -r linux-x64 -o compiled\linux-x64
+dotnet publish -r win-x64 -o compiled\win-x64

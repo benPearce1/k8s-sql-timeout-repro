14f3409fc989594e8317a89aaf93e4f368af6990
Pawel Pabich <pawel@pabich.eu>
2020-02-10 12:50:26 +1000

    Sequence of queries from the deployment

--- source/reprocli/Program.cs
+++ source/reprocli/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.Data.SqlClient;
 
 namespace reprocli
@@ -13,35 +15,125 @@ namespace reprocli
     {
         static void Main(string[] args)
         {
-            var count = int.TryParse(args[0], out int c) ? c : 100;
-            var connString = args[1];
+            try
+            {
+                var count = int.TryParse(args[0], out int c) ? c : 100;
+                var connString = args[1];
 
-            var total = Stopwatch.StartNew();
+                var total = Stopwatch.StartNew();
 
-            Enumerable.Range(0, count)
-                        .AsParallel()
-                        .WithDegreeOfParallelism(10)
-                        .ForAll(number => Scenario2(connString, number));
+               /* Enumerable.Range(0, count)
+                            .AsParallel()
+                            .WithDegreeOfParallelism(10)
+                            .ForAll(number => Scenario2(connString, number));
 
-            total.Stop();
+                total.Stop();
+                Console.WriteLine($"Scenario 2 Total: {total.Elapsed}");
 
-            Console.WriteLine($"Scenario 2 Total: {total.Elapsed}");
+                total.Restart();
+                Enumerable.Range(0,count)
+                    .AsParallel()
+                    .WithDegreeOfParallelism(10)
+                    .ForAll(n => Scenario3(connString, n));
+                Console.WriteLine($"Scenario 3 total: {total.Elapsed}"); */
 
-            total.Restart();
-            Enumerable.Range(0,count)
-                .AsParallel()
-                .WithDegreeOfParallelism(10)
-                .ForAll(n => Scenario3(connString, n));
-            Console.WriteLine($"Scenario 3 total: {total.Elapsed}");
-                
-            //await Scenario1(connString, count);
-            //Scenario2(connString, count);
+
+                var queries = GetQueries();
+                total.Restart();
+                Enumerable.Range(0,count)
+                    .AsParallel()
+                    .WithDegreeOfParallelism(count)
+                    .ForAll(n => Scenario4(connString, n, queries));
+                Console.WriteLine($"Scenario 4 total (simulate 1 deployment): {total.Elapsed}");
+
+
+                //await Scenario1(connString, count);
+                //Scenario2(connString, count);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
+
+
+        static string[] GetQueries()
+        {
+            var document = new XmlDocument();
+            document.PreserveWhitespace = true;
+            document.Load(@"SQL.xml");
+            var queries = new List<string>();
+
+            var nodes = document.GetElementsByTagName("Event");
+
+            foreach(var node in nodes.Cast<XmlNode>())
+            {
+                var children = node.ChildNodes.Cast<XmlNode>().ToArray();
+                var sql = children.SingleOrDefault(chnode => chnode.Name == "Column" && chnode.Attributes.Cast<XmlAttribute>().Any(at => at.Name == "name" && at.Value == "TextData"));
+
+                if (sql != null)
+                {
+                    if (!sql.InnerText.StartsWith("exec sp_reset_connection") &&
+                        !sql.InnerText.StartsWith("-- network protocol: TCP/IP") &&
+                        !sql.InnerText.Contains("INSERT") &&
+                        !sql.InnerText.Contains("UPDATE") &&
+                        !sql.InnerText.Contains("DELETE"))
+
+                    {
+                        queries.Add(sql.InnerText);
+                    }
+                }
+            }
+
+            return queries.ToArray();
+        }
+
+        private static void Scenario4(string connString, int number, string[] queries)
+        {
+            var totalTenantStopWatch = Stopwatch.StartNew();
+
+            var buffer = new object[100];
+            for (var i = 0; i < queries.Length; i++)
+            {
+                var queryStopWatch = Stopwatch.StartNew();
+
+                var query = queries[i];
+                using (var connection = new SqlConnection(connString))
+                {
+                    connection.Open();
+                    using (var transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+                    {
+                        using (var command = new SqlCommand(query, connection, transaction))
+                        {
+                            using (var reader = command.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    reader.GetValues(buffer);
+                                }
+                            }
+                        }
+
+                        transaction.Commit();
+                    }
+                }
+
+                queryStopWatch.Stop();
+                Console.WriteLine($"Number: {number}. Query: {i} Time: {queryStopWatch.Elapsed}");
+            }
+
+            totalTenantStopWatch.Stop();
+            Console.WriteLine($"Number: {number}. All Queries. Time: {totalTenantStopWatch.Elapsed}");
+        }
+
 
         private static void Scenario3(string connString, int n)
         {
-            SqlDataReader reader2 = null;  
-            string query1 =   
+            SqlDataReader reader2 = null;
+            string query1 =
                 @"Select [Id]
                 ,[OwnerId]
                 ,[Version]
@@ -49,7 +141,7 @@ namespace reprocli
                 ,[JSON]
                 ,[RelatedDocumentIds]
                 ,[SpaceId]
-                ,[OwnerType] From VariableSet Where OwnerId = 'Deployments-2809'";  
+                ,[OwnerType] From VariableSet Where OwnerId = 'Deployments-2809'";
             string query2 = "SELECT Id From [VariableSet] Where OwnerId = 'Deployments-2806'";
 
             using (SqlConnection awConnection = new SqlConnection(connString))
@@ -65,8 +157,8 @@ namespace reprocli
                     {
                         Console.WriteLine(reader1["OwnerId"]);
 
-                        // The following line of code requires  
-                        // a MARS-enabled connection.  
+                        // The following line of code requires
+                        // a MARS-enabled connection.
                         reader2 = cmd2.ExecuteReader();
                         using (reader2)
                         {
